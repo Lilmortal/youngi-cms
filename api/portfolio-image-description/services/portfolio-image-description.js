1eb57abb74abe2a83aa17164ce40278655759dbe
Jack Tan <jacktan165@gmail.com>
2020-05-16 21:05:05 +1200

    portfolio image description query by name

--- api/portfolio-image-description/services/portfolio-image-description.js
+++ api/portfolio-image-description/services/portfolio-image-description.js
@@ -1,8 +1,14 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/services.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  async fetch(params, name) {
+    return strapi
+      .query("Portfolio-image-description")
+      .findOne({ name: params.name });
+  },
+};
