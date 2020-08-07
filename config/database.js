050ea8a1e60cf1e92761058a63cfb609ec0fe3aa
Jack Tan <jacktan165@gmail.com>
2020-08-07 16:20:29 +1200

    add dev and prod database config

--- config/database.js
+++ config/database.js
@@ -1,20 +1,40 @@
-module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "bookshelf",
-      settings: {
-        client: "postgres",
-        host: env("DATABASE_HOST"),
-        port: env.int("DATABASE_PORT"),
-        username: env("DATABASE_USERNAME"),
-        password: env("DATABASE_PASSWORD"),
-        database: env("DATABASE_NAME"),
-        ssl: { rejectUnauthorized: false },
+module.exports = ({ env }) => {
+  if (env("NODE_ENV") === "production") {
+    return {
+      defaultConnection: "default",
+      connections: {
+        default: {
+          connector: "bookshelf",
+          settings: {
+            client: "postgres",
+            host: env("DATABASE_HOST"),
+            port: env.int("DATABASE_PORT"),
+            username: env("DATABASE_USERNAME"),
+            password: env("DATABASE_PASSWORD"),
+            database: env("DATABASE_NAME"),
+            ssl: { rejectUnauthorized: false },
+          },
+          options: {
+            debug: true,
+          },
+        },
       },
-      options: {
-        debug: true,
+    };
+  }
+
+  return {
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "bookshelf",
+        settings: {
+          client: "sqlite",
+          filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+        },
+        options: {
+          useNullAsDefault: true,
+        },
       },
     },
-  },
-});
+  };
+};
