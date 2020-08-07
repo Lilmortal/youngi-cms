b86610d705388653385b1f44c834ab26de502919
Jack Tan <jacktan165@gmail.com>
2020-08-07 14:55:10 +1200

    added default port

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ module.exports = ({ env }) => ({
       settings: {
         client: "postgres",
         host: "${process.env.DATABASE_HOST}",
-        port: "${process.env.DATABASE_PORT}",
+        port: "${process.env.DATABASE_PORT}" || 5432,
         username: "${process.env.DATABASE_USERNAME}",
         password: "${process.env.DATABASE_PASSWORD}",
         database: "${process.env.DATABASE_NAME}",
