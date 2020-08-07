0cb6039ea0857ff2dd50c71d31a6e4fb2f5ace06
Jack Tan <jacktan165@gmail.com>
2020-08-07 14:22:12 +1200

    dont reject unauthorized ssl

--- config/database.js
+++ config/database.js
@@ -10,7 +10,7 @@ module.exports = ({ env }) => ({
         username: "${process.env.DATABASE_USERNAME}",
         password: "${process.env.DATABASE_PASSWORD}",
         database: "${process.env.DATABASE_NAME}",
-        schema: "public",
+        ssl: { rejectUnauthorized: false },
       },
       options: {
         debug: true,
