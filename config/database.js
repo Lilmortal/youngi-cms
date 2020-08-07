535cb21d398c3bca0ccf443e3bb1202edee1b7d7
Jack Tan <jacktan165@gmail.com>
2020-08-07 13:48:13 +1200

    migrate sqlite database to postgres

--- config/database.js
+++ config/database.js
@@ -1,14 +1,19 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: "postgres",
+        host: "${process.env.DATABASE_HOST}",
+        port: "${process.env.DATABASE_PORT}",
+        username: "${process.env.DATABASE_USERNAME}",
+        password: "${process.env.DATABASE_PASSWORD}",
+        database: "${process.env.DATABASE_NAME}",
+        schema: "public",
       },
       options: {
-        useNullAsDefault: true,
+        debug: true,
       },
     },
   },
