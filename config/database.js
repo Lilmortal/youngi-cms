470f2e661dd4639d6bcb34447c203a0f6fad626c
Jack Tan <jacktan165@gmail.com>
2020-08-07 15:07:08 +1200

    convert database port to int

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ module.exports = ({ env }) => ({
       settings: {
         client: "postgres",
         host: "${process.env.DATABASE_HOST}",
-        port: "${process.env.DATABASE_PORT}" || 5432,
+        port: parseInt("${process.env.DATABASE_PORT}"),
         username: "${process.env.DATABASE_USERNAME}",
         password: "${process.env.DATABASE_PASSWORD}",
         database: "${process.env.DATABASE_NAME}",
